aef32fc2c2c8851924fd646051d5412c58abe8eb
Peta <peta.maj82@gmail.com>
2015-11-02 10:52:04 +0100

    Ignore files updated. llvm.h added.

---
+++ llvm.h
@@ -0,0 +1,30 @@
+#ifndef LLVM_H
+#define LLVM_H
+
+#pragma warning(push, 0)
+#pragma warning(disable : 4996)
+#include <llvm/IR/Verifier.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Analysis/Passes.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include <llvm/ExecutionEngine/ObjectCache.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Path.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/Scalar.h> 
+#pragma warning(pop)
+
+
+#endif
