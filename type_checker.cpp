3244bfca19eb8f23e71037cc94956d120c495284
oli <o@o1o.ch>
2015-11-12 18:38:34 +0100

    also fix indent in type checker

--- type_checker.cpp
+++ type_checker.cpp
@@ -5,48 +5,48 @@ using namespace llvm;
 
 namespace rift {
 
-    char TypeChecker::ID = 0;
+char TypeChecker::ID = 0;
 
-    bool TypeChecker::runOnFunction(llvm::Function & f) {
-        state.clear();
-        do {
-            state.iterationStart();
-            for (auto & b : f) {
-                for (auto & i : b) {
-                    if (CallInst * ci = dyn_cast<CallInst>(&i)) {
-                        StringRef s = ci->getCalledFunction()->getName();
-                        if (s == "doubleVectorLiteral") {
-                            state.upddate(ci, Type::D);
-                        } else if (s == "fromDoubleVector") {
-                            state.upddate(ci, Type::D);
-                        } else if (s == "characterVectorLiteral") {
-                            state.upddate(ci, Type::C);
-                        } else if (s == "fromCharacterVector") {
-                            state.upddate(ci, Type::C);
-                        } else if (s == "genericSub") {
-                            Type t1 = state.get(ci->getOperand(0));
-                            Type t2 = state.get(ci->getOperand(1));
-                            switch (t1) {
-                                case Type::D:
-                                case Type::T:
-                                    switch (t2) {
-                                        case Type::D:
-                                        case Type::T:
-                                            state.upddate(ci, Type::D);
-                                            break;
-                                        default:
-                                            throw "Invalid types for binary subtraction";
-                                    }
-                                    break;
-                                default:
-                                    throw "Invalid types for binary subtraction";
-                            }
+bool TypeChecker::runOnFunction(llvm::Function & f) {
+    state.clear();
+    do {
+        state.iterationStart();
+        for (auto & b : f) {
+            for (auto & i : b) {
+                if (CallInst * ci = dyn_cast<CallInst>(&i)) {
+                    StringRef s = ci->getCalledFunction()->getName();
+                    if (s == "doubleVectorLiteral") {
+                        state.upddate(ci, Type::D);
+                    } else if (s == "fromDoubleVector") {
+                        state.upddate(ci, Type::D);
+                    } else if (s == "characterVectorLiteral") {
+                        state.upddate(ci, Type::C);
+                    } else if (s == "fromCharacterVector") {
+                        state.upddate(ci, Type::C);
+                    } else if (s == "genericSub") {
+                        Type t1 = state.get(ci->getOperand(0));
+                        Type t2 = state.get(ci->getOperand(1));
+                        switch (t1) {
+                            case Type::D:
+                            case Type::T:
+                                switch (t2) {
+                                    case Type::D:
+                                    case Type::T:
+                                        state.upddate(ci, Type::D);
+                                        break;
+                                    default:
+                                        throw "Invalid types for binary subtraction";
+                                }
+                                break;
+                            default:
+                                throw "Invalid types for binary subtraction";
                         }
                     }
                 }
             }
-        } while ( not state.hasReachedFixpoint());
-        return false;
-    }
+        }
+    } while ( not state.hasReachedFixpoint());
+    return false;
+}
 
 }
--- type_checker.h
+++ type_checker.h
@@ -7,94 +7,94 @@
 namespace rift {
 
 
-    /** The point of the typechecker is to check that the program conforms to the very basic set of rules. These are by far not exhaustive, but they will catch at least something.
+/** The point of the typechecker is to check that the program conforms to the very basic set of rules. These are by far not exhaustive, but they will catch at least something.
 
-    The only types you should worry about are D for double vectors, C for character vectors and F for functions. T is the top element of the lattice, meaning that the type of the RVal is uknown.
+  The only types you should worry about are D for double vectors, C for character vectors and F for functions. T is the top element of the lattice, meaning that the type of the RVal is uknown.
 
-    There is some example code in the implementation of runOnFunction() method, we have prefilled the fromDoubleVector, doubleVectorLiteral and genericSub runtime calls. Your task is to extend this functionality further, including the implementation of phi nodes, where two values merge into one. For this, you will need to fill in proper ordering in the method lessThan.
-
-
-    */
+  There is some example code in the implementation of runOnFunction() method, we have prefilled the fromDoubleVector, doubleVectorLiteral and genericSub runtime calls. Your task is to extend this functionality further, including the implementation of phi nodes, where two values merge into one. For this, you will need to fill in proper ordering in the method lessThan.
 
 
-    class TypeChecker : public llvm::FunctionPass {
+*/
+
+
+class TypeChecker : public llvm::FunctionPass {
+public:
+
+    enum Type {
+        B,
+        D,
+        C,
+        F,
+        T,
+    };
+
+
+
+    class MachineState {
     public:
-
-        enum Type {
-            B,
-            D,
-            C,
-            F,
-            T,
-        };
-
-
-
-        class MachineState {
-        public:
-            Type get(llvm::Value * v) {
-                if (type.count(v))
-                    return type.at(v);
-                else
-                    return Type::B;
-            }
-
-            void upddate(llvm::Value * v, Type t) {
-                Type prev = get(v);
-                if (prev < t) {
-                    type[v] = t;
-                    changed = true;
-                }
-            }
-
-            void clear() {
-                type.clear();
-            }
-
-            void iterationStart() {
-                changed = false;
-            }
-
-            bool hasReachedFixpoint() {
-                return !changed;
-            }
-
-            MachineState() : changed(false) {}
-
-            friend std::ostream & operator << (std::ostream & s, MachineState & m);
-
-        private:
-
-            bool changed;
-
-            std::map<llvm::Value *, Type> type;
-
-        };
-
-
-
-        static char ID;
-
-        char const * getPassName() const override {
-            return "TypeChecker";
+        Type get(llvm::Value * v) {
+            if (type.count(v))
+                return type.at(v);
+            else
+                return Type::B;
         }
 
-        TypeChecker() : llvm::FunctionPass(ID) {}
+        void upddate(llvm::Value * v, Type t) {
+            Type prev = get(v);
+            if (prev < t) {
+                type[v] = t;
+                changed = true;
+            }
+        }
 
-        bool runOnFunction(llvm::Function & f);
+        void clear() {
+            type.clear();
+        }
+
+        void iterationStart() {
+            changed = false;
+        }
+
+        bool hasReachedFixpoint() {
+            return !changed;
+        }
+
+        MachineState() : changed(false) {}
+
+        friend std::ostream & operator << (std::ostream & s, MachineState & m);
 
     private:
-        MachineState state;
+
+        bool changed;
+
+        std::map<llvm::Value *, Type> type;
 
     };
 
 
 
+    static char ID;
 
-    inline bool operator < (TypeChecker::Type t1, TypeChecker::Type t2) {
-        // fill me in
-        return false;
+    char const * getPassName() const override {
+        return "TypeChecker";
     }
+
+    TypeChecker() : llvm::FunctionPass(ID) {}
+
+    bool runOnFunction(llvm::Function & f);
+
+private:
+    MachineState state;
+
+};
+
+
+
+
+inline bool operator < (TypeChecker::Type t1, TypeChecker::Type t2) {
+    // fill me in
+    return false;
+}
 
 
 } // namespace rift
