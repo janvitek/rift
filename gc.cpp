53804091a99eb0cb3b8c6acd67adc037ee4483ca
oli <o@o1o.ch>
2015-11-18 15:51:39 +0100

    make sure unboxed values are gc'd as well

--- gc.cpp
+++ gc.cpp
@@ -10,7 +10,7 @@ namespace gc {
 template<>
 void GarbageCollector::markImpl<RVal>(RVal * value) {
     if (value->type == RVal::Type::Function)
-        mark(value->f->env);
+        mark(value->f);
     // Otherwise leaf node, nothing to do
 };
 
@@ -22,6 +22,21 @@ void GarbageCollector::markImpl<Environment>(Environment * env) {
     if (env->parent)
         mark(env->parent);
 }
+
+template<>
+void GarbageCollector::markImpl(RFun * fun) {
+    mark(fun->env);
+};
+
+template<>
+void GarbageCollector::markImpl(DoubleVector * value) {
+    // Leaf node, nothing to do
+};
+
+template<>
+void GarbageCollector::markImpl(CharacterVector * value) {
+    // Leaf node, nothing to do
+};
 
 
 /*
