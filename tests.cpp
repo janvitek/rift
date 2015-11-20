11a179cc0ca32a8ea8bf1268cb0e23ae800a48ac
peta <peta.maj82@gmail.com>
2015-11-20 10:14:45 +0100

    Tests for the class project implemented.

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,14 @@
 #include "runtime.h"
 #include "tests.h"
+#include "type_analysis.h"
 
 using namespace std;
 using namespace rift;
 namespace rift {
 
 #define TEST(code, ...) test(__LINE__, code, new RVal({__VA_ARGS__}))
+#define HAS_RUNTIME_CALL(code, name) testFunctionContains(__LINE__, code, name)
+#define HAS_NOT_RUNTIME_CALL(code, name) testFunctionNotContains(__LINE__, code, name)
 
     void test(int line, char const * source, RVal * expected) {
         try {
@@ -26,13 +29,11 @@ namespace rift {
         }
     }
 
-    typedef bool (*FunChecker)(llvm::Function *, char const *);
-
     bool containsRuntimeCall(llvm::Function * f, char const * name) {
-        for (auto & b : f) {
+        for (auto & b : *f) {
             for (auto & i : b) {
-                if (CallInst * ci = dyn_cast<CallInst>(&i)) {
-                    StringRef s = ci->getCalledFunction()->getName();
+                if (llvm::CallInst * ci = llvm::dyn_cast<llvm::CallInst>(&i)) {
+                    llvm::StringRef s = ci->getCalledFunction()->getName();
                     if (s == name)
                         return true;
                 }
@@ -41,11 +42,27 @@ namespace rift {
         return false;
     }
 
-    void testFunctionContains(int line, char const * source, char const *( )) {
+    void testFunctionContains(int line, char const * source, char const * name) {
         try {
             Environment * env = new Environment(nullptr);
             RVal * actual = eval(env, source);
-            actual->f->bitcode;
+            if (not containsRuntimeCall(actual->f->bitcode, name))
+                throw "Runtime call not found";
+        } catch (char const * e) {
+            cout << "ERROR at line " << line << " : " << e << endl;
+            cout << source << endl << endl;
+        } catch (...) {
+            cout << "ERROR at line " << line << " : " << endl;
+            cout << source << endl << endl;
+        }
+    }
+
+    void testFunctionNotContains(int line, char const * source, char const * name) {
+        try {
+            Environment * env = new Environment(nullptr);
+            RVal * actual = eval(env, source);
+            if (containsRuntimeCall(actual->f->bitcode, name))
+                throw "Runtime call found";
         } catch (char const * e) {
             cout << "ERROR at line " << line << " : " << e << endl;
             cout << source << endl << endl;
@@ -56,9 +73,86 @@ namespace rift {
     }
 
 
+    /** Passing this means you emit proper runtime call for the generic dot operator.
+     */
+    void project1() {
+        HAS_RUNTIME_CALL("function(a, b) { a %*% b }", "genericDot");
     }
 
 
+    /** These tests check that the generic runtime function for the dot product works properly.
+     */
+    void project2() {
+        TEST("(f = function(a, b) { a %*% b })(c(1,2,3), c(3,2,1))", 10);
+        TEST("(f = function(a, b) { a %*% b })(c(1,2,3), 3)", 18);
+        TEST("(f = function(a, b) { a %*% b })(10, 2)", 20);
+        TEST("(f = function(a, b) { a %*% b })(c(1,2,3,4), c(5,6))", 5 + 6 * 2 + 3 * 5 + 4 * 6);
+    }
+
+    /** Checks that a dot operator result type is correctly set to be double scalar by the type analysis. */
+    void project3() {
+        char const * source = "function(a, b) { a %*% b }";
+        try {
+            Environment * env = new Environment(nullptr);
+            RVal * actual = eval(env, source);
+            llvm::Function * f = actual->f->bitcode;
+            // run type analysis on the function
+            //auto pm = llvm::legacy::FunctionPassManager(f->getParent());
+            TypeAnalysis ta;
+            ta.runOnFunction(*f);
+            // check the type of the genericDot intrinsic call
+            for (auto & b : *f) {
+                for (auto & i : b) {
+                    if (llvm::CallInst * ci = llvm::dyn_cast<llvm::CallInst>(&i)) {
+                        llvm::StringRef s = ci->getCalledFunction()->getName();
+                        if (s == "genericDot") {
+                            AType * t = ta.state.get(ci);
+                            if (t->kind != AType::Kind::R or not t->isScalar())
+                                throw "Generic dot does not have correct type";
+                            if (ta.state.getLocation(t) != ci)
+                                throw "Location attached to the generic dot type is invalid";
+                            t = t->payload;
+                            if (ta.state.getLocation(t) != nullptr)
+                                throw "Generic dot payload cannot have location";
+                            t = t->payload;
+                            if (ta.state.getLocation(t) != nullptr)
+                                throw "Generic dot payload cannot have location";
+                            return;
+                        }
+                    }
+                }
+            }
+            throw "Generic dot not found";
+        } catch (char const * e) {
+            cout << "ERROR at line " << __LINE__ << " : " << e << endl;
+            cout << source << endl << endl;
+        } catch (...) {
+            cout << "ERROR at line " << __LINE__ << " : " << endl;
+            cout << source << endl << endl;
+        }
+
+
+
+    }
+
+    /** Tests that the double vector specialization is properly inserted by the optimization and that it performs properly.
+     */
+    void project4() {
+        HAS_RUNTIME_CALL("function() { c(1,2) %*% c(3,4) }", "doubleDot");
+        HAS_RUNTIME_CALL("function() { c(1,2, 3) %*% 5 }", "doubleDot");
+        HAS_NOT_RUNTIME_CALL("function() { c(1,2) %*% c(3,4) }", "genericDot");
+        HAS_NOT_RUNTIME_CALL("function() { c(1,2, 3) %*% 5 }", "genericDot");
+        TEST("c(1,2) %*% c(3,4)", 11);
+        TEST("c(1,2) %*% 3", 9);
+    }
+
+    /** Tests that the double scalar specialization works and that double or generic dot calls are not emitted.
+     */
+    void project5() {
+        HAS_NOT_RUNTIME_CALL("function() { 3 %*% 4 }", "genericDot");
+        HAS_NOT_RUNTIME_CALL("function() { 3 %*% 4 }", "doubleDot");
+        TEST("3 %*% 4", 12);
+    }
 
     void tests() {
         cout << "Running tests..." << endl;
@@ -129,6 +223,12 @@ namespace rift {
         TEST("a = c(1, 2, 3) a[1]", 2);
         TEST("a = \"aba\" a[c(0,2)]", "aa");
         TEST("a = c(1,2,3) a[c(0,1)] = 56 a", 56, 56, 3);
+
+        project1();
+        project2();
+        project3();
+        project4();
+        project5();
     }
 
 } // namespace rift
