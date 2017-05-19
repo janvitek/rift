83f28c8dc85209743deb64076fdd40de881080d1
o <o@o1o.ch>
2017-05-19 09:52:15 +0200

    fix export script to exclude the local cmake config

--- export.rb
+++ export.rb
@@ -9,13 +9,14 @@ end
 
 usage if ARGV.size != 2
 
+HOME = File.dirname(__FILE__)
+
 FILES = ['CMakeLists.txt', 'LICENSE', 'local', 'src', 'README.md', 'tests',
          '.gitignore']
+IGNORE = [/local\/cmake.cmake$/, /.*swp/]
 
 EXPORT_DIR = ARGV[1]
 VERSION = Integer(ARGV[0])
-
-HOME = File.dirname(__FILE__)
 
 if Dir.exists? EXPORT_DIR
     puts "#{EXPORT_DIR} already exists. Overwriting? (y/n)"
@@ -36,33 +37,41 @@ def crawl(f)
     src = File.join(HOME, f)
     trg = File.join(EXPORT_DIR, f)
 
-    if File.directory?(src)
-        FileUtils.mkdir_p(trg)
-        Dir.foreach(src) do |g|
-            next if g == '.' or g == '..'
-            crawl(File.join(f,g))
-        end
-    else
-        output = ""
-        print = [true]
-        File.readlines(src).each do |line|
-            if line =~ /#if (VERSION (<=|>=|<|>|==) \d+)/
-                print << (eval($1) && print.last)
-            elsif line =~ /#endif .*VERSION/
-                if (print.size == 1)
-                    puts "Unbalanced endif in #{src}"
-                    exit 1
-                end
-                print.pop
-            else
-                output << line if print.last
+    IGNORE.each do |i|
+        return if i =~ src
+    end
+    begin
+        if File.directory?(src)
+            FileUtils.mkdir_p(trg)
+            Dir.foreach(src) do |g|
+                next if g == '.' or g == '..'
+                crawl(File.join(f,g))
             end
+        else
+            output = ""
+            print = [true]
+            File.readlines(src).each do |line|
+                if line =~ /#if (VERSION (<=|>=|<|>|==) \d+)/
+                    print << (eval($1) && print.last)
+                elsif line =~ /#endif .*VERSION/
+                    if (print.size == 1)
+                        puts "Unbalanced endif in #{src}"
+                        exit 1
+                    end
+                    print.pop
+                else
+                    output << line if print.last
+                end
+            end
+            if (print.size != 1)
+                puts "Unclosed endif in #{src}"
+                exit 1
+            end
+            File.open(trg, 'w') { |file| file.write(output) } if output != ""
         end
-        if (print.size != 1)
-            puts "Unclosed endif in #{src}"
-            exit 1
-        end
-        File.open(trg, 'w') { |file| file.write(output) } if output != ""
+    rescue
+        puts "Error in File #{f}"
+        exit 1
     end
 end
 
